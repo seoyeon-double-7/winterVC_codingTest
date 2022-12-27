6abb0f34fa61df507185c74e720324419f4b75ae
Mirim <s2103@e-mirim.hs.kr>
2022-12-27 13:27:07 +0900

    for문과 스트림을 활용 해서 배열의 평균값 구하기

---
+++ src/day2/ArrayAverage.java
@@ -0,0 +1,27 @@
+package day2;
+
+import java.util.*;
+
+public class ArrayAverage {
+    public static void main(String args[]) {
+        int numbers1[] = {1,2,3,4,5,6,7,8};
+        int numbers2[] = {1,2,3,4,5,6,7,8};
+
+        double answer = 0;
+
+        // 1. for문을 이용한 방법
+        
+        int nLeng = numbers1.length;
+        for(int i=0; i<numbers1.length; i++){
+            answer += numbers1[i];
+        }
+        answer/=nLeng;
+        System.out.println("for문 이용 : "+answer);
+
+        // 스트림
+        answer = Arrays.stream(numbers1)
+                .average()
+                .orElse(0);
+        System.out.println("스트림 이용 : "+answer);
+    }
+}
