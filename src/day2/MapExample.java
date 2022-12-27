718ddbab65819e442c67ac5f19f0e3262aa2d274
Mirim <s2103@e-mirim.hs.kr>
2022-12-27 10:18:37 +0900

    요소변환 메소드 asDoubleStream(), boxed(), flatMap()

---
+++ src/day2/MapExample.java
@@ -0,0 +1,39 @@
+package day2;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.IntStream;
+
+public class MapExample {
+
+    public static void main(String args[]) {
+
+        // asDoubleStream
+
+        int[] intArray = {1, 2, 3, 4, 5};
+
+        IntStream intStream = Arrays.stream(intArray);
+        intStream
+                .asDoubleStream()   // double로 바꿔주기
+                .forEach(d -> System.out.println(d));    // 최종출력
+        System.out.println("================================");
+
+
+        // boxed (많이 쓰임)
+
+        intStream = Arrays.stream(intArray);
+        intStream
+                .boxed()    //기본형 자료형 -> 랩퍼 오브젝트형으로
+                .forEach(obj -> System.out.println(obj.intValue())); // 숫자형으로 값을 표현
+        System.out.println("================================");
+
+
+        // flatMap
+
+        List<String> inputList1 = Arrays.asList("java8 Lamda", "stream mapping");
+        inputList1.stream()
+                .flatMap(data -> Arrays.stream(data.split(" ")))
+                .forEach(word -> System.out.println(word));
+
+    }
+}
